ba65b6d4dcd84e5727e20ec9ba53a86814309962
Adam W. Willis <return.of.octobot@gmail.com>
2020-01-14 01:43:58 -0500

    techpack: audio: Tone down repetitive logging
    These represent some of our heaviest offenders.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    [nem0-z: Disable some more loggers]
    Signed-off-by: Zlatan Radovanovic <zlatan.radovanovic@fet.ba>

--- techpack/audio/asoc/codecs/tfa9894/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa9894/tfa98xx.c
@@ -2985,8 +2985,7 @@ static int tfa98xx_mute(struct snd_soc_dai *dai, int mute, int stream)
 
 		cancel_delayed_work_sync(&tfa98xx->init_work);
 
-
-		pr_err("%s: tfa98xx->dsp_fw_state = %d\n", __func__, tfa98xx->dsp_fw_state);
+		pr_debug("%s: tfa98xx->dsp_fw_state = %d\n", __func__, tfa98xx->dsp_fw_state);
 
 		if (tfa98xx->dsp_fw_state != TFA98XX_DSP_FW_OK)
 			return 0;
