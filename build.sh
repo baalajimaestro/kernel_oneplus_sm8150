0e8a35d0fcdc3fb284f4150d719671200c7b2b07
baalajimaestro <me@baalajimaestro.me>
2021-05-11 14:28:34 +0000

    sempahore: Clone toolchains outside the kernel dir
    Signed-off-by: GitHub <noreply@github.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-git clone https://github.com/arter97/arm64-gcc.git --quiet -b master --depth=1 gcc
-git clone https://github.com/arter97/arm32-gcc.git --quiet -b master --depth=1 gcc32
-git clone https://github.com/baalajimaestro/AnyKernel3.git --depth=1 anykernel3
-export CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-elf-"
-export CROSS_COMPILE_ARM32="$(pwd)/gcc32/bin/arm-eabi-"
+git clone https://github.com/arter97/arm64-gcc.git --quiet -b master --depth=1 /home/baalajimaestro/gcc
+git clone https://github.com/arter97/arm32-gcc.git --quiet -b master --depth=1 /home/baalajimaestro/gcc32
+git clone https://github.com/baalajimaestro/AnyKernel3.git --quiet --depth=1 /home/baalajimaestro/anykernel3
+export CROSS_COMPILE="/home/baalajimaestro/gcc/bin/aarch64-elf-"
+export CROSS_COMPILE_ARM32="/home/baalajimaestro/gcc32/bin/arm-eabi-"
 export ARCH=arm64
 export SUBARCH=arm64
 make -j$(nproc) sm8150_defconfig O=out
