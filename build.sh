3134ce7426fbcceb23d69585968d8fd858583e9e
baalajimaestro <me@baalajimaestro.me>
2021-05-11 14:30:34 +0000

    semaphore: Avoid exporting variables
    Signed-off-by: GitHub <noreply@github.com>

--- build.sh
+++ build.sh
@@ -3,9 +3,5 @@
 git clone https://github.com/arter97/arm64-gcc.git --quiet -b master --depth=1 /home/baalajimaestro/gcc
 git clone https://github.com/arter97/arm32-gcc.git --quiet -b master --depth=1 /home/baalajimaestro/gcc32
 git clone https://github.com/baalajimaestro/AnyKernel3.git --quiet --depth=1 /home/baalajimaestro/anykernel3
-export CROSS_COMPILE="/home/baalajimaestro/gcc/bin/aarch64-elf-"
-export CROSS_COMPILE_ARM32="/home/baalajimaestro/gcc32/bin/arm-eabi-"
-export ARCH=arm64
-export SUBARCH=arm64
-make -j$(nproc) sm8150_defconfig O=out
-make -j$(nproc) O=out
+make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="/home/baalajimaestro/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="/home/baalajimaestro/gcc32/bin/arm-eabi-" sm8150_defconfig 
+make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="/home/baalajimaestro/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="/home/baalajimaestro/gcc32/bin/arm-eabi-"
