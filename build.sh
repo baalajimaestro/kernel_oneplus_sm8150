569669fcb2dbccdfd2267050c7347c4a21ddaf8f
baalajimaestro <me@baalajimaestro.me>
2021-05-11 15:03:41 +0000

    semaphore: Export toolchains into PATH
    Signed-off-by: GitHub <noreply@github.com>

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 git clone https://github.com/arter97/arm64-gcc.git --quiet -b master --depth=1 /home/baalajimaestro/gcc
 git clone https://github.com/arter97/arm32-gcc.git --quiet -b master --depth=1 /home/baalajimaestro/gcc32
 git clone https://github.com/baalajimaestro/AnyKernel3.git --quiet --depth=1 /home/baalajimaestro/anykernel3
-make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="/home/baalajimaestro/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="/home/baalajimaestro/gcc32/bin/arm-eabi-" sm8150_defconfig 
-make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="/home/baalajimaestro/gcc/bin/aarch64-elf-" CROSS_COMPILE_ARM32="/home/baalajimaestro/gcc32/bin/arm-eabi-"
+export PATH="/home/baalajimaestro/gcc/bin:/home/baalajimaestro/gcc32/bin:$PATH"
+make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="aarch64-elf-" CROSS_COMPILE_ARM32="arm-eabi-" sm8150_defconfig 
+make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="aarch64-elf-" CROSS_COMPILE_ARM32="arm-eabi-"
