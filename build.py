40994f053378f07f95e0bebcf65eecd9263eeecc
baalajimaestro <me@baalajimaestro.me>
2021-05-12 10:07:17 +0000

    semaphore: Code fixes
    Signed-off-by: GitHub <noreply@github.com>

--- build.py
+++ build.py
@@ -16,12 +16,13 @@ async def send_message(user_id: int, text: str):
     message = await bot.send_message(user_id, text, disable_notification=False, disable_web_page_preview=True, parse_mode=types.ParseMode.MARKDOWN_V2)
     return message
 
-async def edit_message(user_id: int, text: str, message: str):
+async def edit_message(user_id: int, text: str, message):
     old_text =  message.md_text
-    await message.edit_text(old_text + '\n' + text, disable_web_page_preview=True, parse_mode=types.ParseMode.MARKDOWN_V2)
+    message = await message.edit_text(old_text + '\n' + text, disable_web_page_preview=True, parse_mode=types.ParseMode.MARKDOWN_V2)
+    return message
 
 def execute(cmd):
-    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
     for stdout_line in iter(popen.stdout.readline, ""):
         print(stdout_line, end = '')
         with open("build.log", 'a') as file:
@@ -52,7 +53,7 @@ async def runner():
         anykernel3 = Repo.clone_from("https://github.com/baalajimaestro/AnyKernel3", "/home/baalajimaestro/AnyKernel3", branch='master')
         print("Retarded CI: Downloaded Necessary Dependencies")
         message = "`Downloaded Dependenices....`"
-        await edit_message(518221376, message, message_track)
+        message_track = await edit_message(518221376, message, message_track)
 
         defconfig = 'make -j' + str(os.cpu_count()) + ' O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="aarch64-elf-" CROSS_COMPILE_ARM32="arm-eabi-" sm8150_defconfig'
         make = 'make -j' + str(os.cpu_count()) + ' O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="aarch64-elf-" CROSS_COMPILE_ARM32="arm-eabi-"'
@@ -67,28 +68,32 @@ async def runner():
         if return_code != 0:
             import requests
             log = []
-            with open("build.log", "r") as file:
-                log = file.readlines()
+            with open("build.log", "r") as file1:
+                log = file1.readlines()
             str1 = ""
-            str1.join(log)
+            for i in log[-3000:]:
+                str1 += i
             nekobin_key = requests.post("https://nekobin.com/api/documents", json={"content": str1}).json()["result"]["key"]
             await edit_message(518221376, "[Build Log](https://nekobin.com/" + nekobin_key + ")", message_track)
             raise RuntimeError
 
         message = "`Started Make....`"
-        await edit_message(518221376, message, message_track)
+        message_track = await edit_message(518221376, message, message_track)
 
         return_code= execute(list_make)
         if return_code != 0:
             import requests
             log = []
-            with open("build.log", "r") as file:
-                log = file.readlines()
+            with open("build.log", "r") as file1:
+                log = file1.readlines()
             str1 = ""
-            str1.join(log)
+            for i in log[-3000:]:
+                str1 += i
             nekobin_key = requests.post("https://nekobin.com/api/documents", json={"content": str1}).json()["result"]["key"]
-            await edit_message(518221376, "[Build Log](https://nekobin.com/" + nekobin_key + ")", message_track)
+            message_track = await edit_message(518221376, "[Build Log](https://nekobin.com/" + nekobin_key + ")", message_track)
             raise RuntimeError
+
+        message_track = await edit_message(518221376, "`Build Succeeded\!`", message_track)
 
     except:
         print("Our Build Failed, but your traceback should help you!")
