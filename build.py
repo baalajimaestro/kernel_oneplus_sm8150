f30e3ea31dd5d6e53cdad7bece6f12fb32a690c0
baalajimaestro <me@baalajimaestro.me>
2021-05-12 04:44:09 +0000

    semaphore: Switch to python build script
    Signed-off-by: GitHub <noreply@github.com>

---
+++ build.py
@@ -0,0 +1,65 @@
+from asyncio import create_subprocess_exec as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+
+import asyncio
+import logging
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import exceptions, executor
+
+API_TOKEN = os.environ.get("BOT_API_KEY")
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger('broadcast')
+
+bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.MARKDOWN_V2)
+dp = Dispatcher(bot)
+
+async def send_message(user_id: int, text: str):
+    message = await bot.send_message(user_id, text, disable_notification=False, parse_mode=types.ParseMode.MARKDOWN_V2)
+    return message
+
+async def edit_message(user_id: int, text: str, message: str, parse_mode=types.ParseMode.MARKDOWN_V2):
+    old_text =  message.md_text
+    await message.edit_text(old_text + '\n' + text)
+
+async def runner():
+    from git import Repo
+    import sys
+    import os
+    message = "`Retarded CI`\n"
+    message += "`Spinning Build for commit:` [" + os.environ.get("SEMAPHORE_GIT_SHA")[:6] + "](https://github.com/baalajimaestro/kernel_oneplus_sm8150/commit/" + os.environ.get("SEMAPHORE_GIT_SHA") +")\n"
+    message += "`Downloading Dependenices....`"
+    message_track = await send_message("518221376", message)
+    gcc = Repo.clone_from("https://github.com/arter97/arm64-gcc", "/home/baalajimaestro/gcc", branch='master')
+    gcc32 = Repo.clone_from("https://github.com/arter97/arm32-gcc", "/home/baalajimaestro/gcc32", branch='master')
+    anykernel3 = Repo.clone_from("https://github.com/baalajimaestro/AnyKernel3", "/home/baalajimaestro/AnyKernel3", branch='master')
+    message = "`Downloaded Dependenices....`"
+    await edit_message("518221376", message, message_track)
+    sys.path.append("/home/baalajimaestro/gcc/bin")
+    sys.path.append("/home/baalajimaestro/gcc32/bin")
+    defconfig = 'make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="aarch64-elf-" CROSS_COMPILE_ARM32="arm-eabi-" sm8150_defconfig'
+    make = 'make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="aarch64-elf-" CROSS_COMPILE_ARM32="arm-eabi-"'
+    
+    process = await asyncio.create_subprocess_exec(
+        defconfig.strip(),
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
+    print(result)
+    await asyncio.sleep(10)
+
+    process = await asyncio.create_subprocess_exec(
+        defconfig.strip(),
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+
+    stdout, stderr = await process.communicate()
+    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
+    print(result)
+
+if __name__ == '__main__':
+    executor.start(dp, runner())
--- build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-git clone https://github.com/arter97/arm64-gcc.git --quiet -b master --depth=1 /home/baalajimaestro/gcc
-git clone https://github.com/arter97/arm32-gcc.git --quiet -b master --depth=1 /home/baalajimaestro/gcc32
-git clone https://github.com/baalajimaestro/AnyKernel3.git --quiet --depth=1 /home/baalajimaestro/anykernel3
-export PATH="/home/baalajimaestro/gcc/bin:/home/baalajimaestro/gcc32/bin:$PATH"
-export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
-make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="aarch64-elf-" CROSS_COMPILE_ARM32="arm-eabi-" sm8150_defconfig 
-make -j$(nproc) O=out ARCH=arm64 SUBARCH=arm64 CROSS_COMPILE="aarch64-elf-" CROSS_COMPILE_ARM32="arm-eabi-"
