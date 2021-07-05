afd927cb74125ce375d81426041cad8879e71339
baalajimaestro <me@baalajimaestro.me>
2021-07-05 05:49:07 +0000

    [SQUASH]: Add support for building on semaphore  ci
    Detailed commits on GCC branch
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+cp /home/baalajimaestro/kernel_oneplus_sm8150/out/arch/arm64/boot/Image.gz-dtb /home/baalajimaestro/AnyKernel3
+cd /home/baalajimaestro/AnyKernel3
+export ZIPNAME="Retarded-Kernel-CI-$(date +%Y%m%d).zip"
+zip -r9 "${ZIPNAME}" -- *
+ZIPURL=$(curl -sL bashupload.com -T ${ZIPNAME} | grep wget)
+echo "DOWNLOAD: $ZIPURL"
+curl -s -X POST https://api.telegram.org/bot"${TELEGRAM_TOKEN}"/sendMessage -d text="${ZIPURL}" -d chat_id="518221376"
+echo "If you attempt to download the bashupload link, please dont ask for support"
+echo "These are untested builds which are built automatically on push!"
+echo "Expect NO WARRANTY of any sort if you land on data crashes/bootloops"
